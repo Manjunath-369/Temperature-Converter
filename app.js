dcade79f920d95d666baf825ed9796c656ce71e4
Manjunath-369 <149147639+Manjunath-369@users.noreply.github.com>
2024-01-15 18:03:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const user = document.querySelector(".insideText");
+const toFar = document.getElementById("Celcious");
+const toCelci = document.getElementById("FarrFahrenheite");
+const result = document.querySelector(".result");
+const but = document.querySelector(".buttonType");
+let temp;
+function convert(){
+    if(toFar.checked){
+        temp = (Number(user.value) * (9/5)) + 32
+        result.textContent = `${temp} F`;
+    }
+    else if(toCelci.checked){
+        temp = (5/9)*(Number(user.value)-32);
+        result.textContent = `${temp} C`;
+    }
+}
